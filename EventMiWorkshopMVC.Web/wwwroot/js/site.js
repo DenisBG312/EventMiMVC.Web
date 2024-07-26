ad399ed2e3ec29996df3068b58a8bb7ec3d3f836
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2024-07-26 22:54:19 +0300

    added design

--- EventMiWorkshopMVC.Web/wwwroot/js/site.js
+++ EventMiWorkshopMVC.Web/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    // Smooth scrolling for internal links
+    $("a[href^='#']").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
